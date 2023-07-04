2ce0c6212c3bd4ea872d79ca5209e8143afbcc33
Daniel Miquiles <danielvictormiquiles@gmail.com>
2023-07-04 12:12:52 -0300

    feat: aluno entity anotations

--- src/main/java/me/dio/academia/digital/entity/Aluno.java
+++ src/main/java/me/dio/academia/digital/entity/Aluno.java
@@ -4,18 +4,34 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Data;
+
+@Entity
+@Data
 public class Aluno {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String nome;
 
+  @Column(unique = true)
   private String cpf;
 
   private String bairro;
 
   private LocalDate dataDeNascimento;
 
+  @OneToMany(mappedBy = "aluno", cascade = CascadeType.ALL)
   private List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
 
 }
